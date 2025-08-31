7db7b86ef96c8c6096d723e15b3f9f6dbf82576f
JUNG SEUNGHOON <seuthootdev@gmail.com>
2025-09-01 01:05:56 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -804,7 +804,7 @@ export default function Resume() {
                 <CardDescription>대한적십자사혈액관리본부</CardDescription>
               </CardHeader>
               <CardContent>
-                <p className="text-sm text-muted-foreground">68회 / 272시간</p>
+                <p className="text-sm text-muted-foreground">71회 / 284시간</p>
               </CardContent>
             </Card>
             <Card className="shadow-lg hover:shadow-xl transition-shadow duration-300">
